8b51823d59ca6233c53d364ef24bedea76fb3527
Sirikam <ben.dik@live.no>
2017-03-14 15:26:43 +0100

    løeger til groupsingroups

---
+++ src/model/GroupsInGroups.java
@@ -0,0 +1,26 @@
+package model;
+
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class GroupsInGroups {
+
+    private SimpleStringProperty parentGroup;
+    private SimpleStringProperty childGroup;
+
+    public GroupsInGroups(String parentGroup, String childGroup){
+        this.parentGroup = new SimpleStringProperty(parentGroup);
+        this.childGroup = new SimpleStringProperty(childGroup);
+    }
+
+    public String getInsertQuery(){
+       String q = "Insert into GroupsInGroups (parentGroup, childGroup) " +
+               "values('"+getParentGroup()+"', '"+getChildGroup()+"')";
+       return q;
+    }
+
+    public String getParentGroup(){return parentGroup.get();}
+
+    public String getChildGroup(){return childGroup.get();}
+}
