766c41691432ed5b6d01a7060e29ccbbc3dca37c
Sirikam <ben.dik@live.no>
2017-03-14 15:11:47 +0100

    legger til  groups i model

---
+++ src/model/Groups.java
@@ -0,0 +1,21 @@
+package model;
+
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Groups {
+
+    private SimpleStringProperty groupName;
+
+    public Groups(String groupName){
+        this.groupName = new SimpleStringProperty(groupName);
+    }
+
+    public String getInsertQuery() {
+        String q = "Insert into Groups (groupName) Values ("+ getGroupName() + ")";
+        return q;
+    }
+
+    public String getGroupName(){return groupName.get();}
+}
