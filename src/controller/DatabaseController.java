5437fab65f3d20abf344be9422e3d3263f14006f
margreli <margreli@stud.ntnu.no>
2017-03-16 02:00:31 +0100

    added methods for inserting sessions

--- src/controller/DatabaseController.java
+++ src/controller/DatabaseController.java
@@ -1,13 +1,21 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 import model.Database;
+import model.Indoor;
+import model.Outdoor;
+import model.Session;
 
 public class DatabaseController {
 
 		private Database db;
 		private ControllerManager manager;
+		private PreparedStatement pstmt;
 		
 		public DatabaseController(ControllerManager manager) {
 			this.manager = manager;
@@ -40,4 +48,82 @@ public class DatabaseController {
 		public ArrayList<String> selectAction(String query, String type){
 			return db.select(query, type);
 		}
+
+		private void insertSession(Session session, Connection conn) {
+		    String q = "INSERT INTO Session(startDateAndTime, endDateAndTime, inOrOut, personalShape, prestation, note) VALUES (?,?,?,?,?,?);";
+		    try {
+                pstmt = conn.prepareStatement(q);
+                pstmt.setTimestamp(1, new Timestamp(session.getStartDate().getTime()));
+                pstmt.setTimestamp(2, new Timestamp(session.getEndDate().getTime()));
+                pstmt.setString(3, session.getInOrOut());
+                pstmt.setInt(4, session.getPersonalShape());
+                pstmt.setInt(5, session.getPrestation());
+                pstmt.setString(6, session.getNote());
+                pstmt.executeUpdate();
+            } catch (SQLException se) {
+		        se.printStackTrace();
+            } finally {
+                try{
+                    if(this.pstmt != null)
+                        this.pstmt.close();
+                } catch(SQLException se){
+                    se.printStackTrace();
+                }
+
+            }
+
+
+        }
+
+        public void insertIndoorSession(Indoor session){
+            db.connectToDB();
+            Connection conn = db.getConnection();
+            System.out.println(conn.toString());
+            insertSession(session, conn);
+            String q = "INSERT INTO indoorsession(indoorStartDateAndTime, aircondition, viewers) VALUES (?,?,?);";
+            try {
+                pstmt = conn.prepareStatement(q);
+                pstmt.setTimestamp(1, new Timestamp(session.getStartDate().getTime()));
+                pstmt.setString(2, session.getAircondition());
+                pstmt.setInt(3,session.getViewers());
+                pstmt.executeUpdate();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } finally {
+                try{
+                    if(this.pstmt != null)
+                        this.pstmt.close();
+                } catch(SQLException se){
+                    se.printStackTrace();
+                }
+            }
+            db.closeConnection();
+
+        }
+
+    public void insertOutdoorSession(Outdoor session){
+        db.connectToDB();
+        Connection conn = db.getConnection();
+        System.out.println(conn.toString());
+        insertSession(session, conn);
+        String q = "INSERT INTO outdooression(outdoorStartDateAndTime, temperature, weather) VALUES (?,?,?);";
+        try {
+            pstmt = conn.prepareStatement(q);
+            pstmt.setTimestamp(1, new Timestamp(session.getStartDate().getTime()));
+            pstmt.setInt(2, session.getTemperature());
+            pstmt.setString(3,session.getWeather());
+            pstmt.executeUpdate();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } finally {
+            try{
+                if(this.pstmt != null)
+                    this.pstmt.close();
+            } catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        db.closeConnection();
+
+    }
 }
